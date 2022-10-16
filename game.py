7f04e3bfb4e9d08bcdaa49999bb76922ae1124cd
Serhiy <97760349+Whyfe@users.noreply.github.com>
2022-10-16 12:41:43 +0300

    First step
    Adding main window

---
+++ game.py
@@ -0,0 +1,20 @@
+
+from pygame import 8
+
+class GameSprite (sprite.Sprite):
+    def __init__ (self):
+        pass
+    def reset (self):
+        pass
+class Player (GameSprite):
+    update_right (self):
+        pass
+    update_left (self):
+        pass
+win_width = 600
+win_height = 500
+
+window = win.set_mode ((win_width, win_height))
+
+BLACK = (0, 0, 0)
+window.fill (BLACK)
